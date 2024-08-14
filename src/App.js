be4f2f1d18206154342d8527d99dd68811200134
Mayank <kumar113349@gmail.com>
2024-08-14 14:06:48 +0530

    1st, 22

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        This is a class based component
+      </div>
+    )
+  }
+}
