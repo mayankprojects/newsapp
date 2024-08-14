82c2fa81bc3c2f0b95cdbbd3e11abd4862ab225e
Mayank <kumar113349@gmail.com>
2024-08-14 15:18:37 +0530

    3rd 24

--- src/components/News.js
+++ src/components/News.js
@@ -1,14 +1,29 @@
-import React, { Component } from 'react'
-import Newsitem from './Newsitem'
+import React, { Component } from "react";
+import Newsitem from "./Newsitem";
 
 export class News extends Component {
   render() {
     return (
       <div>
-        <Newsitem/>
+        <>
+          <div className="container my-3">
+            <h2 className="my-4">These are the top headlines</h2>
+            <div className="row">
+              <div className="col-md-4">
+                <Newsitem title="My Title" description="My Description" />
+              </div>
+              <div className="col-md-4">
+                <Newsitem title="My Title" description="My Description" />
+              </div>
+              <div className="col-md-4">
+                <Newsitem title="My Title" description="My Description" />
+              </div>
+            </div>
+          </div>
+        </>
       </div>
-    )
+    );
   }
 }
 
-export default News
+export default News;
--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -1,13 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class Newsitem extends Component {
   render() {
+    let {title, description} = this.props;
+
     return (
-      <div>
-        This is a new item
-      </div>
-    )
+      <>
+        <div className="card" style={{width: "18rem"}}>
+          <img src="..." className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">
+              {description}
+            </p>
+            <a href="#" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+      </>
+    );
   }
 }
 
-export default Newsitem
+export default Newsitem;
